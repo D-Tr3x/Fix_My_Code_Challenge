b890e331397629a516386554d76d8192e1cf413e
D-Tr3x <owosenidaniel13@gmail.com>
2025-02-18 13:57:16 +0100

    Fixed naming errors, instantiation, consistency, spellings in square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
+"""Module defines a Square class"""
 
-class square():
-    
+
+class Square:
+    """ Class representing a square (or rectangle) """
     width = 0
     height = 0
 
-    
+
     def __init__(self, *args, **kwargs):
+        self.width = 0
+        self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
+    def area(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -22,7 +27,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.area())
+    print(s.perimeter())
